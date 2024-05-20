4c5f558e8048fb0c843a7354568077a007ecd0e7
chanaka <chanakadrathnayaka@gmail.com>
2024-05-19 18:48:10 -0700

    A log is removed

--- src/controllers/exchange.controller.ts
+++ src/controllers/exchange.controller.ts
@@ -2,7 +2,6 @@ import {Request, Response} from "express";
 import {FinnHub} from "../configs/finnhub.config";
 
 const marketStatus = (req: Request, res: Response) => {
-  console.log(req.params.market)
   FinnHub.getClient().marketStatus(req.params.market, (error: any, data: any, response: any) => {
     console.log(data);
     console.error(error);
